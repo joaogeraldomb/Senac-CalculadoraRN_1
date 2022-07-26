85884186614c6eb0ed3f4c645d57e375e7540fe1
João Geraldo <jonyyys.p@hotmail.com>
2022-07-25 23:29:11 -0300

    corrigido o wireframe

--- App.js
+++ App.js
@@ -74,12 +74,12 @@ export default function App() {
 
         {buttons.map((button) => 
           button === '=' ? // Mapeamento do botão =
-        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#3dd0e3'}]}>
+        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#1e1240'}]}>
           <Text style={[styles.textButton, {color: "white", fontSize: 30}]}>{button}</Text>
         </TouchableOpacity>
           : // Mapeamento dos outros botões
           <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'black': '#0093a6'}]}>{button}</Text>
+            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? '#ebe5f0': '#7c787f'}]}>{button}</Text>
           </TouchableOpacity>
         )}
       </View>
@@ -95,17 +95,20 @@ const styles = StyleSheet.create({
   results: {
     flex: 2,
     justifyContent: "center",
-    backgroundColor: "#f5f5f5"
+    //backgroundColor: "#f5f5f5"
+    backgroundColor: "#1e1240"
   },
   resultText: {
-    color: "#282F38",
+    //color: "#282F38",
+    color: "#ffffff",
     fontSize: 32,
     fontWeight: "bold",
     padding: 12,
     textAlign: "right"
   },
   historyText:{
-    color: "#7c7c7c",
+    //color: "#7c7c7c",
+    color: "#64616c",
     fontSize: 20,
     marginRight: 10,
     alignSelf: 'flex-end',
@@ -115,7 +118,8 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: "#3d0075",
+    //backgroundColor: 'green', acertei
     alignItems: 'center',
     justifyContent: 'center',
     minWidth: 90, 
@@ -124,6 +128,7 @@ const styles = StyleSheet.create({
   },
   textButton: {
     color: "#7c7c7c",
-    fontSize: 20,
+    //color: "#f4f0f7", errada
+        fontSize: 20,
   } 
 });
